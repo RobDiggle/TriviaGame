d05841f0b233af079f5003c91725428cb0408d7f
Robert Diggle <Robschach13@yahoo.com>
2017-02-16 18:20:44 -0500

    First commit

---
+++ assets/javascript/app.js.js
@@ -0,0 +1,12 @@
+
+    setTimeout(fiveSeconds, 5000);
+    setTimeout(tenSeconds, 10000);
+    setTimeout(fifteenSeconds, 15000);
+    setTimeout(twentySeconds, 20000);
+    setTimeout(twentyFiveSeconds, 25000);
+    setTimeout(thirtySeconds, 30000);
+    setTimeout(thirtyFiveSeconds, 35000);
+    setTimeout(fortySeconds, 40000);
+    setTimeout(fortyFiveSeconds, 45000);
+
+      function fiveSeconds() {$("#imgCent").html("<img src='box2.jpg'/>")
